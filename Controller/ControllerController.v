9eff60ddef44656a877ba6d477668371474beb18
Connor Murphy <clm135@duke.edu>
2022-03-26 14:27:34 -0400

    initial version of controller interface

---
+++ Controller/ControllerController.v
@@ -0,0 +1,89 @@
+/**
+ *
+ *  Module for interfacing with the Sega Genesis 3-button controller.
+ *  
+ *
+ **/
+
+module ControllerController(
+    // Output
+    buttons,                    // the state of the buttons on the previous frame
+    
+    // Controller pins - ignoring power and ground for now
+    pin0,                       // UP       (in)
+    pin1,                       // DOWN     (in)    
+    pin2,                       // LEFT     (in)    L when select, 0 when ~select
+    pin3,                       // RIGHT    (in)    R when select, 0 when ~select
+    //pin4,                     // +5V      (out)
+    pin5,                       // B/A      (in)    B when select, A when ~select
+    pin6,                       // select   (out)
+    //pin7,                     // GND      (out)
+    pin8,                       // C/Start  (in)    C when select, Start when ~select
+
+    // DFF inputs
+    frclk,                      // frame clock - period is length of one frame
+    en,
+    clr   
+    );
+
+
+    // Output to console
+    output [7:0] buttons;   // {Start, C, B, A, Right, Left, Down, Up}
+
+    // Input from console
+    input frclk, en, clr;
+
+    // Output to controller
+    output pin6;
+
+    // Input from controller
+    input pin0, pin1, pin2, pin3;
+    input pin5, pin8;
+
+
+    // toggle select every frame
+    reg select = 1;
+    always @(posedge frclk) begin
+       select = ~select; 
+    end
+    assign pin6 = select;
+
+
+    // ---- Store previous frame's button input in DFFs ---
+    wire up, down, left, right, a, b, c, start; // controller gives 0 when pressed, 1 when not - want to invert
+    wire slct_en  =  select & en;
+    wire nslct_en = ~select & en; 
+
+    // UP
+    dffe_ref UP(up, pin0, frclk, en, clr);
+    assign buttons[0] = ~up;
+
+    // DOWN
+    dffe_ref DOWN(down, pin1, frclk, en, clr);
+    assign buttons[1] = ~down;
+
+    // LEFT
+    dffe_ref LEFT(left, pin2, frclk, slct_en, clr);
+    assign buttons[2] = ~left;
+
+    // RIGHT
+    dffe_ref RIGHT(right, pin3, frclk, slct_en, clr);
+    assign buttons[3] = ~right;
+
+    // A
+    dffe_ref A(a, pin5, frclk, nslct_en, clr);
+    assign buttons[4] = ~a;
+
+    // B
+    dffe_ref B(b, pin5, frclk, slct_en, clr);
+    assign buttons[5] = ~b;
+
+    // C
+    dffe_ref C(c, pin8, frclk, slct_en, clr);
+    assign buttons[6] = ~c;
+    
+    // START
+    dffe_ref START(start, pin8, frclk, nslct_en, clr);
+    assign buttons[7] = ~start;
+
+endmodule
---
+++ Controller/ControllerController_tb.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+/**
+ *
+ *  Testbench for the ControllerController module.
+ *
+ **/
+
+module ControllerController_tb();
+    // simulated controller button input
+    reg [7:0] buttons_in = 8'b11111111; // starting with no buttons pressed, remembering that the controller asserts 0 when pressed
+
+    // simulated controller output to console
+    wire pin0, pin1, pin2, pin3, pin5, pin6, pin8;
+    assign pin0 = buttons_in[0];
+    assign pin1 = buttons_in[1];
+    assign pin2 = pin6 ? buttons_in[2] : 1'b0;
+    assign pin3 = pin6 ? buttons_in[3] : 1'b0;
+    assign pin5 = pin6 ? buttons_in[5] : buttons_in[4];   // select ? B : A
+    assign pin8 = pin6 ? buttons_in[6] : buttons_in[7];   // select ? C : Start
+
+
+    // button presses detected by console
+    wire [7:0] buttons_det;
+    reg frclk = 0;
+    reg en = 1;
+    reg clr = 0;
+
+    ControllerController ctrlr( buttons_det,
+                                pin0, pin1, pin2, pin3, pin5, pin6, pin8,
+                                frclk, en, clr);
+
+    // clocking
+    reg system_clock = 0;   // system clock will operate much faster than frame clock
+    always
+		    #10 system_clock = ~system_clock;
+
+    reg [31:0] counter = 0;
+	  reg [31:0] CounterLimit = 1;   // for now, say a frame clock cycle is 2 system clock cycles (high for 1, low for 1)
+    always @(posedge system_clock) begin
+        if (counter < CounterLimit)
+            counter = counter + 1;
+        else begin
+            counter <= 0;
+            frclk <= ~frclk;
+        end
+	  end
+
+    reg [8:0] simcounter = 0;
+    always @(posedge frclk) begin
+        #5 // delay to stabilize select value
+        $display("frame %h: s=%b, %b ==> %b\n", simcounter, pin6, buttons_in, buttons_det);
+        simcounter <= simcounter + 1;
+        if (simcounter == 511)
+            $finish;
+        if (simcounter[0] == 0)
+            buttons_in <= buttons_in - 1;
+    end
+
+
+    // waveform
+    initial begin
+        $dumpfile("Controller/Test Files/cc.vcd");
+        $dumpvars(0, ControllerController_tb);
+    end
+
+
+
+endmodule
